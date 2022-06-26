d9f5274b58c8fb26328a7b13e7647b84567c7500
Maksymilian Zadka <maksymilian.j.zadka@gmail.com>
2022-06-26 23:52:20 +0200

    feat: add possibility to set back path

--- app/components/GoBackButton.tsx
+++ app/components/GoBackButton.tsx
@@ -3,11 +3,21 @@ import { useNavigate } from "@remix-run/react";
 import { useTranslation } from "react-i18next";
 import { ArrowBack } from "tabler-icons-react";
 
-export default function GoBackButton({ style }: { style?: any }) {
+export default function GoBackButton({
+  style,
+  goTo,
+}: {
+  style?: any;
+  goTo?: string;
+}) {
   const { t } = useTranslation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <Button leftIcon={<ArrowBack />} style={style} onClick={() => navigate(-1)}>
+    <Button
+      leftIcon={<ArrowBack />}
+      style={style}
+      onClick={() => (goTo ? navigate(goTo) : navigate(-1))}
+    >
       {t("goBack")}
     </Button>
   );
